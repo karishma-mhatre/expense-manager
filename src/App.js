feec0b8a08f7c3aba60b1c561904bb34f9da576c
Apple <karishmamhatre077@gmail.com>
2019-02-13 15:19:01 +0530

    added chart template and dashboard

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './styles/variables.scss';
 import AddExpense from './Components/AddExpense/AddExpense';
 import Header from './Components/Header/Header';
 import AddButton from './Components/AddButton/AddButton';
-import ExpensePieChart from './Components/ExpensePieChart/ExpensePieChart';
-import IncomePieChart from './Components/IncomePieChart/IncomePieChart';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 class App extends Component {
   render() {
@@ -14,10 +13,7 @@ class App extends Component {
         <Header></Header>
         <AddExpense></AddExpense>
         <AddButton></AddButton>
-        <div className="container charts-container">
-          <ExpensePieChart></ExpensePieChart>
-          <IncomePieChart></IncomePieChart>
-        </div>
+        <Dashboard></Dashboard>
       </>
     );
   }
