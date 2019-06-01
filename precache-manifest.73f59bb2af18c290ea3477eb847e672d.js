7f06ec8eec586b29eddfbd4075ba8d56b087f781
Apple <apple@192.168.1.4>
2019-06-01 23:56:27 +0530

    Updates

---
+++ precache-manifest.73f59bb2af18c290ea3477eb847e672d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ba9fe62df962671e03",
+    "url": "/expense-manager/static/js/runtime~main.23ba9fe6.js"
+  },
+  {
+    "revision": "9b25bcf17870ea3f2527",
+    "url": "/expense-manager/static/js/main.9b25bcf1.chunk.js"
+  },
+  {
+    "revision": "eac09e99a6e1ab15a098",
+    "url": "/expense-manager/static/js/1.eac09e99.chunk.js"
+  },
+  {
+    "revision": "9b25bcf17870ea3f2527",
+    "url": "/expense-manager/static/css/main.774038fe.chunk.css"
+  },
+  {
+    "revision": "56e4b2c3f3e3ad46bb06b2acb5fdc8c1",
+    "url": "/expense-manager/index.html"
+  }
+];
